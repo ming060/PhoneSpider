d087c14af4c09caa3ee7efb112cac4716a8eec4a
You-Ming <lym060@gmail.com>
2018-08-05 16:34:00 +0100

    add main

--- Toxl/to_xl.py
+++ Toxl/to_xl.py
@@ -1,14 +1,15 @@
 from openpyxl import Workbook
 import json
 
-wb = Workbook()
-ws = wb.active
-ws.title = "All"
-ws.cell(1, 1, 'Brand')
-ws.cell(1, 2, 'Model')
-ws.cell(1, 3, 'Alias')
+if __name__ == "__main__":
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "All"
+    ws.cell(1, 1, 'Brand')
+    ws.cell(1, 2, 'Model')
+    ws.cell(1, 3, 'Alias')
 
-with open('../PhoneSpider/phones.json') as f:
+    with open('../PhoneSpider/phones.json') as f:
         devices = json.load(f)
         for index, device in enumerate(devices):
             ws.cell(row = index+2, column = 1, value = device['brand'][0])
@@ -17,4 +18,4 @@ with open('../PhoneSpider/phones.json') as f:
             for alias_index, alias in enumerate(aliases):
                 ws.cell(row = index+2, column = alias_index + 3, value = alias)
 
-wb.save('PhoneSpecs.xlsx')
+    wb.save('PhoneSpecs.xlsx')
